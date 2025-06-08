23325a66edba08f2886cb82244ccf83d08479a3b
Ryan Bonifacio <rcbonifacio22@gmail.com>
2025-06-07 22:18:15 -0700

    made changes to get event info from landingPage

--- client/src/components/EventPage.js
+++ client/src/components/EventPage.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import './EventPage.css';
+import { Link, useLocation, useParams } from 'react-router-dom';
 
-const dummyEvent = {
-        title: 'Davis Farmers Market',
-        image: '/images/davis_farmers_market.jpg',
-        paid: false,
-        date: '2025-06-07',
-        description: 'Come enjoy fresh produce and local crafts every Saturday at Central Park!'
-      };
 
 function EventPage(){
+    const eventInfo = useLocation();
+    const event = eventInfo.state?.event;
+    
     return (
         <div className='event-page'>
-            <div className='arrow'>&larr; Go Back</div>
+            <Link to= "/">
+                 <div className='arrow'>&larr; Go Back</div>
+           </Link>
             <div className='event-container'>
-                {dummyEvent.image ? (
-                                <img src ={dummyEvent.image} alt={dummyEvent.title} />
+                {event.image ? (
+                                <img src ={event.image} alt={event.title} />
                             ) : (
                                 <div className='image-placeholder'>[Image]</div>
                             )}
 
                 <div className='event-content'>
-                    <h1 id='event-title'>{dummyEvent.title}</h1>
-                    {dummyEvent.paid ? (
+                    <h1 id='event-title'>{event.title}</h1>
+                    {event.paid ? (
                                         <span id='paid'>Paid</span>
                                     ) : (
                                         <span id='free'>Free</span>
                                     )}
                     
-                    <h1 id='event-date'>Date: {dummyEvent.date}</h1>
-                    <p>Description:<br></br><br></br>{dummyEvent.description}</p>
+                    <h1 id='event-date'>Date: {event.date}</h1>
+                    <p>Description:<br></br><br></br>{event.description}</p>
                 </div>
             </div>
         </div>
